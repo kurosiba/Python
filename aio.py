12cada8ad567a6d9f102c8445d5e81906630dca0
unknown <akito030303@gmail.com>
2023-12-21 21:19:17 +0900

    [add]def & method

---
+++ aio.py
@@ -0,0 +1,11 @@
+from date import time
+
+def main():
+   clock = time()
+   print(clock)
+
+print(__name__)
+
+if __name__=="__main__":
+   main()
+
---
+++ date.py
@@ -0,0 +1,4 @@
+from datetime import datetime 
+
+def time():
+   return __name__
